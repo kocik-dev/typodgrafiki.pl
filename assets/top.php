755bd12c0413563f6aa4617a5faed7524cdfdb9c
typodgrafiki <grzegorz.kocik@o2.pl>
2019-07-29 21:20:45 +0200

    up

--- assets/top.php
+++ assets/top.php
@@ -7,7 +7,6 @@
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
-	        <!-- <small class="txt hidden">MENU</small> -->
 	</button>
 	<?php
 	}

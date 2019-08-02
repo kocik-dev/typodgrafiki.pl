bf64b3a26a20dbba3b4d2ca5187a55f971b88f3f
grzegorz <grzegorz.kocik@o2.pl>
2019-08-02 16:19:22 +0200

    up

--- assets/top-portfolio.php
+++ assets/top-portfolio.php
@@ -1,5 +1,5 @@
 <header>
-	<a id="menuBackButton" href="../portfolio" class="navbar-toggle pull-right">
+	<a id="menuBackButton" href="../" class="navbar-toggle pull-right">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <small class="txt">POWRÓT</small>
--- assets/top.php
+++ assets/top.php
@@ -2,18 +2,10 @@
 	<div id="logo" class="pull-left">
 		<a href="/"><img src="<?php echo BASE_PATH ?>images/logo.png" width="171" height="41" alt="Logo"></a>
 	</div>
-	<?php if ($form_success != 1) { ?>
-	<button id="menuToogleButton" class="navbar-toggle pull-right" type="button" data-toggle="collapse" data-target="#menuToogle" aria-controls="menuToogle" aria-expanded="false" aria-label="Menu navigation">
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	</button>
 	<?php
-	}
-
 	if ($homepage == 1) {
 	?>
-		<a href="kontakt" id="topContactButton" class="btn btn-default pull-right hidden-xs">
+		<a href="kontakt" id="topContactButton" class="btn btn-default pull-right">
 			Napisz do mnie
 			<span></span>
 		</a>
@@ -25,35 +17,11 @@
 	<?php } elseif ($form_success == 1) { ?>
 
 	<?php } else { ?>
-		<a href="../kontakt" id="topContactButton" class="btn btn-default pull-right hidden-xs">
+		<a href="../kontakt" id="topContactButton" class="btn btn-default pull-right">
 			Napisz do mnie
 			<span></span>
 		</a>
 	<?php } ?>
 
 	<div id="top"></div>
-	<div  id="menuToogle" class="nav-menu menu-over text-center">
-		<nav class="nav-menu">
-			<div class="nav-animation">
-				<?php
-				if ($homepage == 1) {
-				?>
-
-					<a href="o-mnie">O <span>M</span>nie</a>
-					<a href="portfolio">Portf<span>o</span>lio</a>
-					<a href="blog" class="disabled">Blog</a>
-					<a href="kontakt">Kon<span>t</span>akt</a>
-
-				<?php } else { ?>
-					
-					<a href="../o-mnie"<?php if ($menu2_active == 1) { ?> class="active"<?php } ?>>O <span>M</span>nie</a>
-					<a href="../portfolio"<?php if ($menu3_active == 1) { ?> class="active"<?php } ?>>Portf<span>o</span>lio</a>
-					<a href="../blog" class="disabled"<?php if ($menu4_active == 1) { ?> class="active"<?php } ?>>Blog</a>
-					<a<?php if ($menu5_active == 1) { ?> class="active"<?php } else{ ?> href="../kontakt"<?php } ?>>Kon<span>t</span>akt</a>
-
-				<?php } ?>
-			</div>
-			
-		</nav>
-	</div>
 </header>

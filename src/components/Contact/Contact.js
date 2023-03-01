27f9eab5aae3c985931438a42f3097786dec2501
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2023-03-01 16:50:39 +0100

    up

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,8 @@
-import React, { createElement, useRef } from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import EmailsjData from './EmailsjData';
 import './Contact.css';
+import './Loader.css';
   
 const Contact = () => {
     
@@ -11,64 +12,93 @@ const Contact = () => {
         e.preventDefault();
         
         const formEl = document.querySelector('.form');
+        const loaderEl = document.createElement('div');
         formEl.classList.add('sending');
         
-        setTimeout(() => {
-            formSended({text: 'OK'});
-        }, "3000");
+        loaderEl.classList.add('lds-roller');
+        formEl.append(loaderEl);
+        loaderEl.innerHTML = '<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>';
+        
+        loaderEl.classList.add('show');
+        
+        
+        // setTimeout(() => {
+        //     formSended({text: 'OK'}, loaderEl);
+        // }, "3000");
         
       
-    //   emailjs.sendForm(EmailsjData.service, EmailsjData.template, form.current, EmailsjData.publicKey)
-    //     .then((result) => {
-    //         formSended(result);
-    //     }, (error) => {
-    //         formSended(error);
-    //     });
+      emailjs.sendForm(EmailsjData.service, EmailsjData.template, form.current, EmailsjData.publicKey)
+        .then((result, loaderEl) => {
+            formSended(result);
+        }, (error, loaderEl) => {
+            formSended(error);
+        });
     };
     
-    const formSended = (res) => {
+    const formSended = (res, loader) => {
+        
+        const formEl = document.querySelector('.form');
+        const formSuccess = document.createElement('div');
+        const formSuccessP = document.createElement('p');
+        const formSuccessButton = document.createElement('a');
+        
+        // loader.classList.remove('show');
+        
+        formSuccessButton.classList.add('btn', 'btn-default');
+        formSuccessButton.setAttribute('id', 'buttonCloseForm');
+        
         if (res.text === 'OK') {
-            const formSuccess = document.createElement('div');
-            const formSuccessButton = document.createElement('button');
-            const formEl = document.querySelector('.form');
-            const formSuccessButtonText = document.createTextNode('Zamknij');
-            
-            formSuccessButton.classList.add('btn', 'btn-default');
-            formSuccessButton.append(formSuccessButtonText);
-            formSuccess.classList.add('form-success');
-            formSuccess.append('Dziękuję za kontakt. Odezwę się tak szybko jak to możliwe.');
-            formSuccess.appendChild(formSuccessButton);
-            formEl.appendChild(formSuccess);
-            
-            // formEl.classList.remove('sending');
-            formEl.reset();
-             
-            {/* <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> */}
-
+            formSuccessP.append('Dziękuję za kontakt. Odpowiem tak szybko jak to możliwe.');
+            formSuccessButton.append('Zamknij');
         }else{
-            
+            formSuccessP.append('Nie udało się wysłać formularza. Spróbuj jeszcze raz.');
+            formSuccessButton.append('Spróbuj ponownie');
         }
+        
+        formSuccess.classList.add('form-success');
+        formSuccess.append(formSuccessP, formSuccessButton);
+        formEl.appendChild(formSuccess);
+        
+        setTimeout(() => {
+            formEl.classList.remove('sending');
+            // loader.remove();
+            if (res.text === 'OK') {   
+                formEl.reset();
+            }
+        }, "1000");
+        
+        formSuccessButton.addEventListener('click', function () {
+            closeformSuccess(formSuccess);
+        });
+        
+    }
+    
+    const closeformSuccess = (el) => {
+        el.style.opacity = 0;
+        setTimeout(() => {
+            el.remove();
+        }, "1000");
     }
     
     return (
         <section id="contact" className="window-height flex horizontal-center">
             <div className="caption small-width">
                 <h2 className="title-small">Kontakt</h2>
-                <p>Dziękuję za poświęcenie mi czasu. Jak mogę Ci dzisiaj pomóc?</p>
                 <form ref={form} onSubmit={sendEmail} className="form grid relative">
-                    <div>
+                    <p className="col-100 form-row">Dziękuję za poświęcenie mi czasu. Jak mogę Ci dzisiaj pomóc?</p>
+                    <div className="form-row">
                         <label>Imię i nazwisko</label>
                         <input type="text" name="form-name" placeholder="Jan Kowalski" required />
                     </div>
-                    <div>
+                    <div className="form-row">
                         <label>Email</label>
                         <input type="email" name="form-email" placeholder="jan@example.com" required />
                     </div>
-                    <div className="messege">
+                    <div className="messege form-row">
                         <label>Wiadomość</label>
-                        <textarea className="form-message" name="message" required></textarea>
+                        <textarea className="form-message" name="message" required ></textarea>
                     </div>
-                    <div className="submit text-right">
+                    <div className="submit text-right form-row">
                         <button type="submit" className="btn btn-default">Wyślij</button>
                     </div>
                 </form>

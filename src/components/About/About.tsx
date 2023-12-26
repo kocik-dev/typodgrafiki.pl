d6eea1658eb66150e4c6d6ce9f0b67097501816c
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2023-12-26 12:33:54 +0100

    change About me section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -10,42 +10,9 @@ const About: React.FC = () => {
         >
             <div className="caption small-width caption-border">
                 <h2 className="title-small">O mnie</h2>
-                <p>
-                    <strong>Front-end developer</strong> o zapleczu graficznym,
-                    a moje główne obszary ekspertyzy to tworzenie warstwy
-                    graficznej w kontekście e-commerce. Z pasją podchodzę do
-                    projektów, które wykorzystują nowoczesne technologie,
-                    szczególnie w obszarze React.
-                </p>
-                <div className="grid vertical-top">
-                    <div>
-                        <h3>Czuję się dobrze w:</h3>
-                        <ul>
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>Bootstrap</li>
-                            <li>SCSS/LESS</li>
-                            <li>JavaScript DOM</li>
-                            <li>jQuery</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Skupiam się na pogłębianiu wiedzy w:</h3>
-                        <ul>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>Typescript</li>
-                            <li>Next</li>
-                        </ul>
-                    </div>
-                </div>
-                <p>
-                    Jestem otwarty na nowe wyzwania i projekty, które pozwolą mi
-                    rozwijać się jako front-end developer. Moją pasją jest
-                    tworzenie estetycznych, responsywnych i przyjaznych dla
-                    użytkownika stron internetowych, które dostarczają wartość
-                    dla klientów.
-                </p>
+                <p>Jako entuzjasta technologii i kreatywny front-end developer, posiadam bogate doświadczenie w projektowaniu warstwy użytkowej dla branży e-commerce. W pracy komercyjnej korzystam z <span className="primary">HTML5/CSS3</span> , <span className="primary">JavaScript DOM</span> , <span className="primary">jQuery</span> , <span className="primary">Bootstrap 3</span> , <span className="primary">LESS</span> używając takich narzędzi jak <span className="primary primary-dark">Visual Studio Code</span> , <span className="primary primary-dark">Terminal</span> , <span className="primary primary-dark">Figma</span> oraz <span className="primary primary-dark">Photoshop</span>.</p>
+                <p>Równolegle do mojej komercyjnej działalności, prowadzę projekt open source, wykorzystujący dodatkowo <span className="primary">Prisma</span>, <span className="primary">Postgresql</span> , <span className="primary">Tailwind CSS</span> , <span className="primary">Javascript</span> , <span className="primary">Typescript</span> i <span className="primary">Next.js</span>. Jest to przestrzeń, gdzie mogę eksperymentować z nowymi technologiami i dzielić się innowacjami.</p>
+                <p>Moje umiejętności w kontroli wersji i zarządzaniu projektami (<span className="primary">SVN</span>, <span className="primary">Github</span>, <span className="primary">Jira</span>) wspierają efektywną współpracę w zespołach. Dążę do tworzenia intuicyjnych, wydajnych i estetycznie przyjemnych interfejsów, które zaspokajają potrzeby biznesowe i zachwycą użytkowników.</p>
                 <a
                     className="btn btn-yellow"
                     href="#portfolio"

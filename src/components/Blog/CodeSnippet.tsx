4f3168908a8a39092a56c50b099388507d92bc4d
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2024-12-02 19:42:55 +0100

    up

--- src/components/Blog/CodeSnippet.tsx
+++ src/components/Blog/CodeSnippet.tsx
@@ -2,7 +2,13 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { okaidia } from "react-syntax-highlighter/dist/cjs/styles/prism"
 import "../../styles/code-snippet.css"
 
-const CodeSnippet = ({ language, code }) => (
+const CodeSnippet = ({
+    language,
+    code,
+}: {
+    language: string
+    code: string
+}) => (
     <>
         <p className="code-language">{language}</p>
         <SyntaxHighlighter language={language} style={okaidia}>

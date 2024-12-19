ada4b93947c56519ec5bc666146b5e404d658e06
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2024-12-19 21:13:39 +0100

    up

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
-// // next.config.js
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     i18n: {
-//         locales: ["en", "pl", "es"], // Dodaj tutaj wszystkie obsługiwane języki
-//         defaultLocale: "en", // Ustaw 'en' jako domyślny język
-//         localeDetection: true, // Włącz automatyczne wykrywanie języka przeglądarki
-//     },
-// }
-
-// module.exports = nextConfig
-
-// const createNextIntlPlugin = require("next-intl/plugin")
-
-// const withNextIntl = createNextIntlPlugin()
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = withNextIntl(nextConfig)
-
 const createNextIntlPlugin = require("next-intl/plugin")
 
 const withNextIntl = createNextIntlPlugin()

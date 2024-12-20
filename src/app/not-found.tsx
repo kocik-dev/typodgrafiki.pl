ac659717962aa9a81a77f6485895492782c3baa2
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2024-12-20 23:00:46 +0100

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import Head from "next/head"
 import "@/styles/App.css"
 import "@/styles/error404.css"
+import { fascinate } from "@/components/Fonts"
 
 export default function Custom404() {
     return (
@@ -11,15 +12,35 @@ export default function Custom404() {
                     You lost! - Grzegorz Kocik - Front-end Developer Portfolio
                 </title>
             </Head>
-            <div className="bg-gradient error404">
-                <div>
-                    <h1>Opps!</h1>
-                    <p>Strona nie została znaleziona.</p>
-                    <Link href="/" className="btn btn-default">
-                        Back to the home page
-                    </Link>
-                </div>
+            <div className="error404">
+                <h1 className={`${fascinate.className}`}>Opps!</h1>
+                <p>Strona nie została znaleziona.</p>
+                <Link href="/" className="btn btn-default">
+                    <Arrow />
+                    Back to the home page
+                </Link>
             </div>
         </>
+    )
+}
+
+const Arrow = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="ionicon"
+            viewBox="0 0 512 512"
+            width={16}
+            height={16}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="48"
+                d="M244 400L100 256l144-144M120 256h292"
+            />
+        </svg>
     )
 }

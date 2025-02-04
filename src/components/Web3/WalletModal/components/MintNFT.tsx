4e25ba01eaa557e33bbaf06317234ee575cda0cf
typodgrafiki <53211722+kocik-dev@users.noreply.github.com>
2025-02-04 15:12:34 +0100

    up

--- src/components/Web3/WalletModal/components/MintNFT.tsx
+++ src/components/Web3/WalletModal/components/MintNFT.tsx
@@ -1,70 +1,75 @@
 "use client"
-
-import {
-    useAccount,
-    useTransaction,
-    usePublicClient,
-    useWalletClient,
-} from "wagmi"
-import { writeContract } from "wagmi/actions"
-import { parseEther } from "viem"
 import { useState } from "react"
 import { IoHammerOutline } from "react-icons/io5"
+import { useAccount, useWriteContract, useChainId, useSwitchChain } from "wagmi"
+import { mainnet, sepolia } from "wagmi/chains"
 
-const CONTRACT_ADDRESS = "0xEb9ea800431966d550526B669766Fe6Ac6021C5A" // Adres kontraktu
-const ABI = [
+const abi = [
     {
-        inputs: [],
         name: "mint",
-        outputs: [],
-        stateMutability: "nonpayable",
         type: "function",
+        stateMutability: "nonpayable",
+        inputs: [],
+        outputs: [],
     },
-]
+] as const
+
+const contractConfig = {
+    address: "0xEb9ea800431966d550526B669766Fe6Ac6021C5A",
+    abi,
+} as const
 
 export default function MintButton() {
-    const { isConnected } = useAccount()
-    const { data: walletClient } = useWalletClient()
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const { address } = useAccount()
+    const chainId = useChainId()
+    const { switchChain } = useSwitchChain()
 
-    const [txHash, setTxHash] = useState<`0x${string}` | null>(null)
-    // const { isLoading, isSuccess } = useTransaction({
-    //     hash: txHash,
-    // })
+    const baseChain = sepolia.id //mainnet.id
 
-    const mintNFT = async () => {
-        // if (!walletClient) return
-        // try {
-        //     const hash = await writeContract(walletClient, {
-        //         address: CONTRACT_ADDRESS,
-        //         abi: ABI,
-        //         functionName: "mint",
-        //         value: parseEther("0"), // Jeśli mint jest darmowy
-        //     })
-        //     setTxHash(hash)
-        // } catch (error) {
-        //     console.error("Minting failed:", error)
-        // }
+    const isWrongNetwork = chainId !== baseChain
+
+    const { writeContract } = useWriteContract()
+
+    const handleMint = async () => {
+        if (isWrongNetwork) {
+            await switchChain({ chainId: baseChain })
+            return
+        }
+
+        try {
+            setIsLoading(true)
+            await writeContract({
+                ...contractConfig,
+                functionName: "mint",
+            })
+            setIsSuccess(true)
+        } catch (err) {
+            console.error("Minting failed:", err)
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
-        // <div>
-        //     <button
-        //         onClick={mintNFT}
-        //         // disabled={!isConnected || isLoading}
-        //         className="bg-blue-500 text-white px-4 py-2 rounded-lg disabled:bg-gray-400"
-        //     >
-        //         {/* {isLoading ? "Minting..." : "Mint NFT"} */}
-        //     </button>
+        <div className="flex gap-1">
+            <button className="btn btn-default">
+                {isWrongNetwork ? "Switch to Ethereum" : "is Ethereum"}
+            </button>
 
-        //     {/* {isSuccess && (
-        //         <p className="text-green-500 mt-2">
-        //             NFT Minted Successfully!
-        //         </p>
-        //     )} */}
-        // </div>
-        <button className="btn btn-default wide-width justify-center">
-            <IoHammerOutline />
-            MINT FOR FREE
-        </button>
+            <button
+                onClick={handleMint}
+                disabled={!address || isLoading || isSuccess || isWrongNetwork}
+                className="btn btn-default wide-width justify-center"
+            >
+                <IoHammerOutline />
+                {isLoading
+                    ? "Minting..."
+                    : isSuccess
+                    ? "Minted!"
+                    : "MINT FOR FREE"}
+            </button>
+        </div>
     )
 }
